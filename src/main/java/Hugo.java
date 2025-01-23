f051456116930af2b3a921df730c197386fa3818
ShengBin-101 <shengbin.chan@gmail.com>
2025-01-23 23:12:20 +0800

    Rename bot to Hugo, add greeting and exit

---
+++ src/main/java/Hugo.java
@@ -0,0 +1,16 @@
+public class Hugo {
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+
+    public static void displayWelcomeMessage() {
+        String welcomeMessage = "____________________________________________________________\n"
+                + "Hello! I'm Hugo\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(welcomeMessage);
+    }
+}
