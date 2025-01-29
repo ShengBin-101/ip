af3d85bdbb3634aee41a94138835f746786a71b3
ShengBin-101 <shengbin.chan@gmail.com>
2025-01-29 15:22:41 +0800

    Add echo, bye

--- src/main/java/Hugo.java
+++ src/main/java/Hugo.java
@@ -1,16 +1,54 @@
+import java.util.Scanner;
+
 public class Hugo {
 
     public static void main(String[] args) {
         displayWelcomeMessage();
+        askInput();
+
+    }
+
+    public static void askInput() {
+        boolean isAskingInput = true;
+        String userInputLine;
+        Scanner scanner = new Scanner(System.in);
+        while (isAskingInput) {
+            userInputLine = scanner.nextLine();
+            switch (userInputLine) {
+            case "bye":
+                displayExitMessage();
+                isAskingInput = false;
+                break;
+            default:
+                echoUserInput(userInputLine);
+                break;
+            }
+        }
+    }
+
+    public static void echoUserInput(String userInput) {
+        String echoMessage = printLine()
+                + userInput + "\n"
+                + printLine();
+        System.out.println(echoMessage.indent(4));
     }
 
     public static void displayWelcomeMessage() {
-        String welcomeMessage = "____________________________________________________________\n"
+        String welcomeMessage = printLine()
                 + "Hello! I'm Hugo\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + printLine();
+        System.out.println(welcomeMessage.indent(4));
+    }
+
+    public static void displayExitMessage() {
+        String exitMessage = printLine()
                 + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(welcomeMessage);
+                + printLine();
+        System.out.println(exitMessage.indent(4));
+    }
+
+    public static String printLine() {
+        return "____________________________________________________________\n";
     }
 }
