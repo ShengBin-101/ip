4f6e54b379ddac25e8f7b886e3ecdfd749b1716d
ShengBin-101 <shengbin.chan@gmail.com>
2025-02-01 22:22:58 +0800

    Implement add event task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,12 +4,29 @@ public class Event extends Task {
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.setFrom(from);
+        this.setTo(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + "[" + getStatusIcon() + "] " + this.description;
+        return "[E]" + "[" + getStatusIcon() + "] " + this.getDescription() +
+                " (from: " + this.getFrom() + " to: " + this.getTo() + ")";
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
     }
 }
--- src/main/java/Hugo.java
+++ src/main/java/Hugo.java
@@ -48,7 +48,6 @@ public class Hugo {
                     isAskingInput = false;
                     break;
                 default:
-//                    addTask(userInputLine);
                     addToDo(inputs);
                     break;
                 }
@@ -110,16 +109,6 @@ public class Hugo {
         }
     }
 
-    // Adding task of no specific type
-    //    public static void addTask(String userInput) {
-    //        Task newTask = new Task(userInput);
-    //        tasks.add(newTask);
-    //        Formatter.printLine();
-    //        Formatter.printMessage("added: " + userInput);
-    //        Formatter.printMessage("Now you have " + tasks.size() + " tasks in the list.");
-    //        Formatter.printLine();
-    //    }
-
     public static void addDeadline(String[] inputs) {
         String taskDescription = "";
         String dueDate = "";
@@ -140,12 +129,12 @@ public class Hugo {
 
         // Extract deadline (words after "/by")
         dueDate = (byIndex != -1 && byIndex < inputs.length - 1)
-                ? String.join(" ", Arrays.copyOfRange(inputs, byIndex+1, inputs.length))
+                ? String.join(" ", Arrays.copyOfRange(inputs, byIndex + 1, inputs.length))
                 : "";
 
         Deadline deadlineTask = new Deadline(taskDescription, dueDate);
         tasks.add(deadlineTask);
-        Formatter.printTaskStatusChange("Got it. I've added this task:",deadlineTask,tasks.size());
+        Formatter.printTaskStatusChange("Got it. I've added this task:", deadlineTask, tasks.size());
     }
 
     public static void addToDo(String[] inputs) {
@@ -155,8 +144,7 @@ public class Hugo {
             if (inputs.length > 1) {
                 taskDescription = String.join(" ", Arrays.copyOfRange(inputs, 1, inputs.length));
             }
-        }
-        else {
+        } else {
             taskDescription = String.join(" ", Arrays.copyOfRange(inputs, 0, inputs.length));
         }
 
@@ -166,9 +154,36 @@ public class Hugo {
     }
 
     public static void addEvent(String[] inputs) {
-        // TODO: extract description
-        // TODO: extract string between /from and /to and string after /to
-        // TODO: Create Event item
+        String taskDescription = "";
+        String from = "";
+        String to = "";
+        int fromIndex = -1;
+        int toIndex = -1;
+
+        if (inputs[0].equals("event")) {
+            if (inputs.length > 1) {
+                // Find the indexes of "/from" and "/to"
+                for (int i = 0; i < inputs.length; i++) {
+                    if (inputs[i].equals("/from")) {
+                        fromIndex = i;
+                    } else if (inputs[i].equals("/to")) {
+                        toIndex = i;
+                    }
+                }
+                taskDescription = String.join(" ", Arrays.copyOfRange(inputs, 1, fromIndex));
+                from = String.join(" ", Arrays.copyOfRange(inputs, fromIndex + 1, toIndex));
+                to = String.join(" ", Arrays.copyOfRange(inputs, toIndex + 1, inputs.length));
+
+                Event event = new Event(taskDescription, from, to);
+                tasks.add(event);
+                Formatter.printTaskStatusChange("Got it. I've added this task:", event, tasks.size());
+
+            } else {
+                Formatter.printLine();
+                Formatter.printMessage("Please enter a event description.");
+                Formatter.printLine();
+            }
+        }
     }
 
     public static void displayWelcomeMessage() {
