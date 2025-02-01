15fd96862a59077ba4c784f6a9bd05dd9ab1c878
ShengBin-101 <shengbin.chan@gmail.com>
2025-02-01 21:43:41 +0800

    Fix variable naming in addDeadline() and refactor askInput()
    Input line should only be split after checking if it is valid (not empty).

--- src/main/java/Hugo.java
+++ src/main/java/Hugo.java
@@ -17,13 +17,13 @@ public class Hugo {
         Scanner scanner = new Scanner(System.in);
         while (isAskingInput) {
             userInputLine = scanner.nextLine();
-            String[] inputs = userInputLine.split(" ");
-            if (inputs[0].equals("")) {
+            if (userInputLine.isEmpty()) {
                 // Handle empty input
                 Formatter.printLine();
                 Formatter.printMessage("Please enter a command or task name to add");
                 Formatter.printLine();
             } else {
+                String[] inputs = userInputLine.split(" ");
                 switch (inputs[0]) {
                 case "deadline":
                     addDeadline(inputs);
@@ -111,20 +111,17 @@ public class Hugo {
     }
 
     // Adding task of no specific type
-    public static void addTask(String userInput) {
-        Task newTask = new Task(userInput);
-        tasks.add(newTask);
-        Formatter.printLine();
-        Formatter.printMessage("added: " + userInput);
-        Formatter.printMessage("Now you have " + tasks.size() + " tasks in the list.");
-        Formatter.printLine();
-    }
+    //    public static void addTask(String userInput) {
+    //        Task newTask = new Task(userInput);
+    //        tasks.add(newTask);
+    //        Formatter.printLine();
+    //        Formatter.printMessage("added: " + userInput);
+    //        Formatter.printMessage("Now you have " + tasks.size() + " tasks in the list.");
+    //        Formatter.printLine();
+    //    }
 
     public static void addDeadline(String[] inputs) {
-        // TODO: extract description
-        // TODO: extract string after /by
-        // TODO: Create Deadline item
-        String description = "";
+        String taskDescription = "";
         String dueDate = "";
         int byIndex = -1;
 
@@ -137,23 +134,21 @@ public class Hugo {
         }
 
         // Extract task description (between "deadline" and "/by")
-        String taskDescription = (byIndex > 1)
+        taskDescription = (byIndex > 1)
                 ? String.join(" ", Arrays.copyOfRange(inputs, 1, byIndex))
                 : "";
 
         // Extract deadline (words after "/by")
-        String deadline = (byIndex != -1 && byIndex < inputs.length - 1)
+        dueDate = (byIndex != -1 && byIndex < inputs.length - 1)
                 ? String.join(" ", Arrays.copyOfRange(inputs, byIndex+1, inputs.length))
                 : "";
 
-        Deadline deadlineTask = new Deadline(taskDescription, deadline);
+        Deadline deadlineTask = new Deadline(taskDescription, dueDate);
         tasks.add(deadlineTask);
         Formatter.printTaskStatusChange("Got it. I've added this task:",deadlineTask,tasks.size());
     }
 
     public static void addToDo(String description) {
-        // TODO: extract description
-        // TODO: Create Todo item
         Todo todoTask = new Todo(description);
         tasks.add(todoTask);
         Formatter.printTaskStatusChange("Got it. I've added this task:", todoTask, tasks.size());
